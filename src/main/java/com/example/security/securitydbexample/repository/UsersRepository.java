e582aacfe8646228b967482205b81a45ba406aa0
koulp <vkoulp@gmail.com>
2018-05-14 10:51:47 +0300

    Initial commit

---
+++ src/main/java/com/example/security/securitydbexample/repository/UsersRepository.java
@@ -0,0 +1,11 @@
+package com.example.security.securitydbexample.repository;
+
+import com.example.security.securitydbexample.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepository extends JpaRepository<Users, Integer> {
+
+    Optional<Users> findByName(String username);
+}
---
+++ src/main/java/com/example/security/securitydbexample/resource/HelloResource.java
@@ -0,0 +1,27 @@
+package com.example.security.securitydbexample.resource;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/rest/hello")
+@RestController
+public class HelloResource {
+
+    @GetMapping("/all")
+    public String hello(){
+        return "Hello You";
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    @GetMapping("/secured/all")
+    public String securedHello(){
+        return "Secured Hello You";
+    }
+
+    @GetMapping("/secured/alternate")
+    public String alternate(){
+        return  "alternate";
+    }
+}
